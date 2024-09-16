aea7faa277d534f2d8c0eb4684c2f370dbb102e2
rafflezs <thi-rafael@hotmail.com.br>
2024-09-16 11:17:24 -0300

    bkp

---
+++ DeepLearning/Projeto-1-MLP/src/models/custom_mlp.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class CustomMLP:
+
+    self.weights = None
+    self.biases = None
+    self.activations = None
+
+    def __init__(self, input_size: list, hidden_layer: dict, output_size: list):
+        pass
+    
+    def activation_functions():
+        pass
